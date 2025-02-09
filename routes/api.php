f74881aec7ca127330aa51766856ea971008d49c
Berkay Çatak <34205493+berkaycatak@users.noreply.github.com>
2025-02-09 15:20:36 +0300

    kan aranıyor duyurusu

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Blood\BloodController;
 use App\Http\Middleware\StorePlayerId;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,5 +17,11 @@ Route::middleware([StorePlayerId::class])->group(function () {
         Route::post("/register", [AuthController::class, "register"])->name("register");
 
         Route::post("/splash", [AuthController::class, "loginCheck"])->name("splash");
+    });
+
+    Route::group(["middleware" => ["auth:sanctum"]], function() {
+        Route::prefix('/blood')->group(function () {
+            Route::post("/request", [BloodController::class, "sendRequest"])->name("send_blood_request");
+        });
     });
 });
